f0063562b74d427064ca2e480a99b4b3331f2fcd
NikCodePro <vermanikhil7838@gmail.com>
2025-02-08 11:18:57 +0530

    update Flask server run command to bind to all interfaces and set port to 5000; adjust fetch URL in index.html for local testing

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello flask server..."
+    return "Hello flask server.."
 
 @app.route('/detect_faces', methods=['POST'])
 def detect_faces():
@@ -63,4 +63,4 @@ def _cors_response(data, status=200):
 
 if __name__ == '__main__':
     # Run the Flask server
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
